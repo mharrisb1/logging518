bd75aad41c781b49e0ec1d6fdf0f2b8fd301f7b5
Michael Harris <michael.harrisru@gmail.com>
2021-07-25 19:14:19 +0000

    Update version

--- tests/test_logging518.py
+++ tests/test_logging518.py
@@ -5,7 +5,7 @@ from logging518.logger518 import default_logging_config, Logger518, __logger518_
 
 
 def test_version():
-    assert __version__ == "0.1.1"
+    assert __version__ == "0.1.2"
 
 
 def test_successful_parsing():
